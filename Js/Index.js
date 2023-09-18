dfb12950fdec2fc47ff899a5de47ea21cbfc52bb
0511Cynthia <223205@ids.upchiapas.edu.mx>
2023-09-18 11:52:12 -0600

    segunda act de js

--- Js/Index.js
+++ Js/Index.js
@@ -1,9 +1,12 @@
-  // Default export is a4 paper, portrait, using millimeters for units
-  function generarPdf(){
-  const doc = new jsPDF();
-  doc.text("Esta es una plantilla", 10, 10);
-  doc.save("Plantillas.pdf");
-}
+async function logMovies() {
+    const response = await fetch(" https://api.jikan.moe/v4/anime?q=naruto&sfw");
+    const movies = await response.json();
+    console.log(movies);
+  }
+  
+  const btn = document.getElementById('btnAnime')
+
+  btn.addEventListener("click", logMovies, false);
 
 
 function aviso(){
@@ -13,23 +16,23 @@ function aviso2(){
     alert('Página no disponible')
 }
 function cambio(){
-    var fondo=document.getElementById('btn1');
-    var fondo2=document.getElementById('btn2');    
-    fondo.style.backgroundColor="#91bcea";
-    fondo2.style.backgroundColor="#91bcea";
+    var fondo=document.getElementById('btn1')
+    var fondo2=document.getElementById('btn2')    
+    fondo.style.backgroundColor="#91bcea"
+    fondo2.style.backgroundColor="#91bcea"
 }
 function verForm(){
-    var ver=document.getElementById('form');
-    ver.style.display="block";
+    var ver=document.getElementById('form')
+    ver.style.display="block"
 }
 function aceptarNombre(){
-    var nombre=document.getElementById('nombre').value;
-    var ver=document.getElementById('form');
+    var nombre=document.getElementById('nombre').value
+    var ver=document.getElementById('form')
     console.log(nombre)
     if (nombre=="") {
-        alert('ingresa el nombre');
+        alert('ingresa el nombre')
     }else{
-        alert('Nombre aceptado');
-        ver.style.display="none";
+        alert('Nombre aceptado')
+        ver.style.display="none"
     }
 }
