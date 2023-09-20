f67a35710ea5ce237e06808152b714cf368a7891
0511Cynthia <223205@ids.upchiapas.edu.mx>
2023-09-20 00:11:59 -0600

    api funcional

--- Js/Index.js
+++ Js/Index.js
@@ -1,12 +1,34 @@
+let animes;
+const imagenes = document.getElementById("imagenes");
+
 async function logMovies() {
-    const response = await fetch(" https://api.jikan.moe/v4/anime?q=naruto&sfw");
+    const response = await fetch("https://api.jikan.moe/v4/anime?q=naruto&sfw");
     const movies = await response.json();
+    const data = movies.data;
     console.log(movies);
+    animes = [data[0], data[1], data[2], data[3]];
+  }
+
+  function renderAnimes(){
+    console.log(animes);
+    animes.map((anime) => { 
+    const animeCard = document.createElement("div");
+    animeCard.classList.add('animeCard');
+    animeCard.innerHTML = `<img src="${anime.images.jpg.image_url}"/>`;
+    imagenes.appendChild(animeCard);
+    });
+  }
+
+  async function getAnimes(){
+    await logMovies();
+    renderAnimes();
   }
   
   const btn = document.getElementById('btnAnime')
 
-  btn.addEventListener("click", logMovies, false);
+  btn.addEventListener("click", getAnimes, false);
+
+
 
 
 function aviso(){
